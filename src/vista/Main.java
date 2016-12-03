4b7cfc01cfc8cd019cf813b05474f78f0bf93471
AdocArg <gonzalocoda@hotmail.com>
2016-12-03 10:11:40 -0300

    Add files via upload

---
+++ src/vista/Main.java
@@ -0,0 +1,65 @@
+package vista;
+
+import controlador.ControladorJuego;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public ControladorJuego controladorJuego;
+	public MenuPrincipalScene menuPrincipal;
+	public NombreJugadoresScene menuNombreJugadores;
+	public CreditosScene creditos;
+	public SeleccionarAlgomonScene algomones;
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Algomón");
+		this.menuPrincipal = new MenuPrincipalScene(new GridPane());
+		this.menuNombreJugadores = new NombreJugadoresScene(new GridPane());
+		this.creditos = new CreditosScene(new VBox());
+		this.algomones = new SeleccionarAlgomonScene(new BorderPane());
+		this.controladorJuego = new ControladorJuego();
+
+		//Estilo de las escenas
+		stage.getStyle().getClass().getResource("style.css");
+		this.menuPrincipal.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		this.creditos.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		this.menuNombreJugadores.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		this.algomones.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		
+		//Cambio de Escenas
+		this.menuNombreJugadores.getBotonAtras().setOnAction(e-> stage.setScene(this.menuPrincipal));
+		this.menuNombreJugadores.getBotonContinuar().setOnAction(e-> stage.setScene(algomones));
+		this.creditos.getBotonAtras().setOnAction(e-> stage.setScene(this.menuPrincipal));
+		this.menuPrincipal.getBotonJugar().setOnAction(e-> stage.setScene(this.menuNombreJugadores));
+		this.menuPrincipal.getBotonCreditos().setOnAction(e->stage.setScene(this.creditos));
+		this.menuPrincipal.getBotonSalir().setOnAction(e-> stage.close());
+		
+		stage.setScene(this.menuPrincipal);
+		stage.show();
+		
+	}
+
+}
