7fb2b834dac6d5f9d72c97731548f220e7ea7b7f
pera21 <pereira.martin21@gmail.com>
2016-12-08 21:48:50 -0300

    Delete Estado.java

--- src/modelo/Estado.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public interface Estado {
-	
-	public void atacar(Algomon atacante, Algomon oponente, FabricaAtaque ataque);
-	public void utilizarElemento(Algomon algomon, Elemento elemento);
-
-}
