ff1d61bae076d0d33b01a674c14b724c03d3dc6c
pera21 <pereira.martin21@gmail.com>
2016-12-08 21:49:23 -0300

    Delete EstadoEfimero.java

--- src/modelo/EstadoEfimero.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public interface EstadoEfimero {
-	public void aplicarEfecto(Algomon algomon) throws estaDormido;
-	public void atacar(Algomon atacante, Algomon oponente, Poder ataque);
-
-}
